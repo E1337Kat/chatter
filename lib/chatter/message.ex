5611cdb50e286afdea19632e214293efe962d323
Ellie Peterson <ekp2h92@gmail.com>
2018-10-20 12:00:52 -0400

    Improved persistence, added history on join.

--- lib/chatter/message.ex
+++ lib/chatter/message.ex
@@ -16,4 +16,9 @@ defmodule Chatter.Message do
     |> cast(attrs, [:name, :message])
     |> validate_required([:name, :message])
   end
+
+  def recent_messages(limit \\ 10) do
+    Chatter.Repo.all(Chatter.Message, limit: limit)
+  end
+
 end
--- lib/chatter_web/channels/chat_room_channel.ex
+++ lib/chatter_web/channels/chat_room_channel.ex
@@ -3,6 +3,7 @@ defmodule ChatterWeb.ChatRoomChannel do
 
   def join("chat_room:lobby", payload, socket) do
     if authorized?(payload) do
+      send(self(), :after_join)
       {:ok, socket}
     else
       {:error, %{reason: "unauthorized"}}
@@ -18,10 +19,28 @@ defmodule ChatterWeb.ChatRoomChannel do
   # It is also common to receive messages from the client and
   # broadcast to everyone in the current topic (chat_room:lobby).
   def handle_in("new_message", payload, socket) do
-    Chatter.Message.changeset(%Chatter.Message{}, payload) |> Chatter.Repo.insert
-    
+    spawn(fn -> save_message(payload) end)
+
     broadcast socket, "new_message", payload
     {:noreply, socket}
+  end
+
+  def handle_info(:after_join, socket) do
+    Chatter.Message.recent_messages()
+    |> Enum.each(fn msg -> push(socket, "new_message", format_msg(msg)) end)
+    {:noreply, socket}
+  end
+
+  defp save_message(message) do
+    Chatter.Message.changeset(%Chatter.Message{}, message)
+      |> Chatter.Repo.insert
+  end
+
+  defp format_msg(msg) do
+    %{
+      name: msg.name,
+      message: msg.message
+    }
   end
 
   # Add authorization logic here as required.
